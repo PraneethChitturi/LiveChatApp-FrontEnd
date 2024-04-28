745261d172a8fde3656d852ae7730ea866ab4002
PraneethChitturi <praneethchitturi12@gmail.com>
2024-04-27 19:33:02 -0700

    IntegratingFrontBack-ResponsiveAlertsSignIn

--- src/redux/slices/app.js
+++ src/redux/slices/app.js
@@ -8,6 +8,11 @@ const initialState = {
         open: false,
         type: "CONTACT", // can be CONTACT, STARRED, SHARED
       },
+    snackbar: {
+        open: null,
+        message: null,
+        severity: null,
+    }
 }
 
 const slice = createSlice({
@@ -17,9 +22,19 @@ const slice = createSlice({
         toggleSidebar(state) {
             state.sidebar.open = !state.sidebar.open;
           },
-          updateSidebarType(state, action) {
+        updateSidebarType(state, action) {
             state.sidebar.type = action.payload.type;
           },
+        openSnackbar(state,action){
+            state.snackbar.open = true;
+            state.snackbar.severity = action.payload.severity;
+            state.snackbar.message = action.payload.message;
+        },
+        closeSnackbar(state,action){
+            state.snackbar.open  = false;
+            state.snackbar.severity = null;
+            state.snackbar.message = null;
+        },
     }
 })
 
@@ -38,3 +53,23 @@ export function UpdateSiderbarType (type) {
         dispatch(slice.actions.updateSidebarType({type} ))
     }
 }
+
+export function showSnackbar ({severity,message}) {
+    return async (dispatch,getState)=>{
+        dispatch(slice.actions.openSnackbar({severity,message}))
+        
+        setTimeout(()=>{
+        dispatch(slice.actions.closeSnackbar());
+    },4000)
+    }
+
+    
+}
+
+export function closeSnackbar () {
+    return async (dispatch,getState)=>{
+        dispatch(slice.actions.closeSnackbar())
+    }
+}
+
+
--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from '../../utils/axios';
+import { showSnackbar } from "./app";
 
 const initialState = {
     isLoggedIn: false,
@@ -50,8 +51,12 @@ export function LoginUser(formValues){
                 isLoggedIn:true,
                 token:response.data.token,
             }))
+            dispatch(showSnackbar({severity:"success",
+                                    message:response.data.message}))
         })).catch(function(error){
             console.log(error);
+            dispatch(showSnackbar({severity:"error",
+                                    message:error.response.data.message}))
         })
     }
 }
